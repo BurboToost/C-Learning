9ec175bceeb746ed2d600945a5a1331cef58ea42
Boivob Sarkar Apon <aagg3992@gmail.com>
2025-03-07 21:55:55 +0600

    Add files via upload

---
+++ escapeSQ.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main(){
+
+
+
+    printf("I\n like\n Pizza!\n");// \n is for new line
+    printf("1\t2\t3\n4\t5\t6\n7\t8\t9\n");// \t is for tab
+    printf("\"I like panta vat\"- Hitler probably");// \ is used for separating "Quote"
+    return 0;
+}
