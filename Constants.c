111021695ab50ea94a31acd0fe5be732cfa472fc
Boivob Sarkar Apon <aagg3992@gmail.com>
2025-03-10 20:04:02 +0600

    Add files via upload

---
+++ Constants.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main(){
+
+
+    const float PI = 3.14159;// const makes sures the value doesnt change
+    // PI = 4.3535;  if i use this then it will show an error
+    printf("%f\n",PI);
+
+
+    return 0;
+}
