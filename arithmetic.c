e0a9506e40784fc65e917b4b152aeeaf312e9d78
Boivob Sarkar Apon <aagg3992@gmail.com>
2025-03-10 20:21:58 +0600

    Add files via upload

---
+++ arithmetic.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+int main(){
+
+    /*
+    + addition
+    - subtraction
+    * multiplication
+    / division
+    % modulus
+    ++ increment
+    -- decrement
+    
+    */
+    int x = 5;
+    int y = 2;
+    //int z = x + y;
+    //int z = x - y;
+    //int z = x * y;
+    //int z = x / (float) y;
+    //int z = x % y;
+    //x++;
+    y--;
+    printf("%d",y);
+     
+
+    
+    //printf("%d",z);
+
+    return 0;
+}
