7de6a4ea9c8c42dd6c262d552bfee25588a89aab
Boivob Sarkar Apon <aagg3992@gmail.com>
2025-03-12 12:37:28 +0600

    Add files via upload

---
+++ MathFunction.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <math.h>// for math
+
+int main(){
+
+double A = sqrt(9); //use %f (square)
+double B = pow(2,4);//use %f (Power- 2 power 4)
+int C = round(3.14);// use %d for printf (round)
+int D = ceil(3.14);//round up %d
+int E = floor(3.99);//round down %d
+double F = fabs(-100);// absolute value use %f
+double G = log(3); // log use %f
+double H = sin(45);// trigeometry use %f
+double I = cos(45);// trigeometry use %f
+double J = tan(45);// l trigeometryuse %f
+
+printf("\n%1f", J);
+
+
+}
