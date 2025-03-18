c1887099c47e89ae98da6fc22336a7dfabd91370
Boivob Sarkar Apon <aagg3992@gmail.com>
2025-03-18 12:28:39 +0600

    Add files via upload

---
+++ ternary-operator.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int findmax(int x, int y)
+{
+/*    if (x > y){
+        return x;
+    }
+    else{
+        return y;
+    }*/
+    return(x > y) ? x : y;
+
+}
+
+int main(){
+
+    // ternary operator = shortcut to if/else when assigning/returning a value
+    // (condition) ? value if true : value if false
+
+    int max = findmax(5, 4);
+    printf("%d", max);
+
+    return 0;
+
+}
