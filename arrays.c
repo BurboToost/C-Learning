1a173a641561f6231e5c82605ace069ff36ee011
Boivob Sarkar Apon <aagg3992@gmail.com>
2025-03-24 20:52:08 +0600

    Add files via upload

---
+++ arrays.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main()
+{
+    // array = a data structure that can store many values of the same data type.
+
+    double prices[] = {5.0, 10.0, 15.0, 20.0, 25.0};
+
+    //char name[] = "Bro";
+
+    printf("%.2lf", prices[3]);
+
+    return 0;
+}
